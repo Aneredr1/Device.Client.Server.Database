70e26bf7d2f7df331338f07f61c984532cadd0a1
Никита Сафронов <kemrussia@gmail.com>
2021-05-04 21:07:48 +0300

    puppets

--- DesktopClient/Model/Message.cs
+++ DesktopClient/Model/Message.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Server.Models
+namespace DesktopClient.Models
 {
     public class Message
     {
@@ -20,15 +20,15 @@ namespace Server.Models
 
         public string group_name { get; set; }
 
-        public Message(DateTime date_action, string code, string code_name, string device_code, string device_name, string group_code, string group_name) 
-        {
-            this.date_action = date_action;
-            this.code = code;
-            this.code_name = code_name;
-            this.device_code = device_code;
-            this.device_name = device_name;
-            this.group_code = group_code;
-            this.group_name = group_name;
-        }
+        //public Message(DateTime date_action, string code, string code_name, string device_code, string device_name, string group_code, string group_name) 
+        //{
+        //    this.date_action = date_action;
+        //    this.code = code;
+        //    this.code_name = code_name;
+        //    this.device_code = device_code;
+        //    this.device_name = device_name;
+        //    this.group_code = group_code;
+        //    this.group_name = group_name;
+        //}
     }
 }
